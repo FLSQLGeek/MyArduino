c5dfc9228bde67ee7c67aee531918d98293a4744
FLSQLGeek <48488764+FLSQLGeek@users.noreply.github.com>
2020-08-23 03:12:53 -0400

    Update NewDataLogger.ino

--- NewDataLogger/NewDataLogger.ino
+++ NewDataLogger/NewDataLogger.ino
@@ -1,3 +1,4 @@
+
 // Arduino data logger with SD card and DHT11 humidity and temperature sensor
 // https://simple-circuit.com/arduino-sd-card-dht11-data-logger/
 
@@ -5,10 +6,10 @@
 #include <SD.h>         // Include SD library
 #include "dht.h"
 File dataFile;
- dht DHT;
+dht DHT;
 #define DHTPIN A0            // DHT11 data pin is connected to Arduino pin 4
 
- 
+const int chipSelect = 4; 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
@@ -16,7 +17,8 @@ void setup() {
   while (!Serial)
     ; // wait for serial port to connect. Needed for native USB port only
   Serial.print("Initializing SD card...");
-  if (!SD.begin()) {
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
     Serial.println("initialization failed!");
     while (1);
   }
@@ -27,20 +29,20 @@ void setup() {
 uint16_t line = 1;
  
 void loop() {
-DHT.read11(DHTPIN);
+    delay(5000);
+    DHT.read11(DHTPIN);
    // Read humidity
   int RH = DHT.humidity;
   //Read temperature in degree Celsius
   int Temp = DHT.temperature;
-  
+ 
   dataFile = SD.open("DHT11Log.txt", FILE_WRITE);
   
   // if the file opened okay, write to it:
-   delay(2000); 
   if (dataFile) {
     Serial.print(line);
     Serial.print(":    Temperature = ");
-    Serial.print((DHT.temperature* 1.8000)+32);
+    Serial.print((Temp * 1.8000)+32);
     Serial.print("°F,    Humidity = ");
     Serial.print(RH);
     Serial.println("%");
@@ -48,7 +50,7 @@ DHT.read11(DHTPIN);
     // Write data to SD card file (DHT11Log.txt)
     dataFile.print(line++);
     dataFile.print(":    Temperature = ");
-    dataFile.print((DHT.temperature* 1.8000)+32);
+    dataFile.print((Temp * 1.8000)+32);
     dataFile.print("°C,    Humidity = ");
     dataFile.print(RH);
     dataFile.println("%");
